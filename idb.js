e037fb9ac24a519790a3dd3d35b22bc22acff25a
Jon Cody <joncodyyy@gmail.com>
2025-12-23 20:53:13 -0800

    added assertName

--- idb.js
+++ idb.js
@@ -10,13 +10,16 @@ const global = (
     )
 );
 
-const indexedDB = global.indexedDB
-    || global.mozIndexedDB
-    || global.webkitIndexedDB
-    || global.msIndexedDB;
+const indexedDB = global.indexedDB || global.mozIndexedDB || global.webkitIndexedDB || global.msIndexedDB;
 
 if (!indexedDB) {
     throw new Error("IndexedDB not supported.");
+}
+
+function assertName(name, label = "Name") {
+    if (typeof name !== "string" || name === "") {
+        throw new TypeError(`${label} must be a non-empty string`);
+    }
 }
 
 const promisify = function (request) {
@@ -28,48 +31,64 @@ const promisify = function (request) {
 
 function client(db) {
     return Object.freeze({
+        clear: function (table) {
+            assertName(table, "Table name");
+            return promisify(db.transaction([table], "readwrite").objectStore(table).clear());
+        },
+        close: () => db.close(),
+        count: function (table, query) {
+            assertName(table, "Table name");
+            return promisify(db.transaction([table], "readonly").objectStore(table).count(query));
+        },
+        delete: function (table, key) {
+            assertName(table, "Table name");
+            return promisify(db.transaction([table], "readwrite").objectStore(table).delete(key));
+        },
         select: function (table, key) {
+            assertName(table, "Table name");
             return promisify(db.transaction([table], "readonly").objectStore(table).get(key));
         },
         selectAll: function (table, query, count) {
+            assertName(table, "Table name");
             return promisify(db.transaction([table], "readonly").objectStore(table).getAll(query, count));
         },
         selectAllKeys: function (table, query, count) {
+            assertName(table, "Table name");
             return promisify(db.transaction([table], "readonly").objectStore(table).getAllKeys(query, count));
         },
         selectIndex: function (table, index, key) {
+            assertName(table, "Table name");
             return promisify(db.transaction([table], "readonly").objectStore(table).index(index).get(key));
         },
-        delete: function (table, key) {
-            return promisify(db.transaction([table], "readwrite").objectStore(table).delete(key));
-        },
         insert: function (table, value, key) {
-            return promisify(
+            assertName(table, "Table name");
+            const tx = db.transaction([table], "readwrite");
+            const store = tx.objectStore(table);
+            const request = (
                 key === undefined
-                ? db.transaction([table], "readwrite").objectStore(table).add(value)
-                : db.transaction([table], "readwrite").objectStore(table).add(value, key)
+                ? store.add(value)
+                : store.add(value, key)
             );
+            return promisify(request);
         },
         update: function (table, value, key) {
-            return promisify(
+            assertName(table, "Table name");
+            const tx = db.transaction([table], "readwrite");
+            const store = tx.objectStore(table);
+            const request = (
                 key === undefined
-                ? db.transaction([table], "readwrite").objectStore(table).put(value)
-                : db.transaction([table], "readwrite").objectStore(table).put(value, key)
+                ? store.put(value)
+                : store.put(value, key)
             );
-        },
-        clear: function (table) {
-            return promisify(db.transaction([table], "readwrite").objectStore(table).clear());
-        },
-        count: function (table, query) {
-            return promisify(db.transaction([table], "readonly").objectStore(table).count(query));
-        },
-        close: () => db.close()
+            return promisify(request);
+        }
     });
 }
 
 function upgrade(db) {
     return Object.freeze({
         create: function (table, options, schema) {
+            assertName(table, "Object store name");
             const store = db.createObjectStore(table, options);
             if (schema) {
                 Object.keys(schema).forEach(function (key) {
@@ -80,17 +99,25 @@ function upgrade(db) {
             return store;
         },
         delete: function (table) {
+            assertName(table, "Object store name");
             db.deleteObjectStore(table);
         }
     });
 }
 
 const idb = {
-    open: function (name, version = 1, fn = function () { return; }) {
+    delete: function (name) {
+        assertName(name, "Database name");
+        return promisify(indexedDB.deleteDatabase(name));
+    },
+    open: function (name, version = 1, fn = function () {
+        return;
+    }) {
         if (typeof version === "function") {
             fn = version;
             version = 1;
         }
+        assertName(name, "Database name");
         const request = indexedDB.open(name, version);
         return new Promise(function (resolve, reject) {
             request.onerror = (e) => reject(e);
@@ -105,9 +132,6 @@ const idb = {
                 }
             };
         });
-    },
-    delete: function (name) {
-        return promisify(indexedDB.deleteDatabase(name));
     }
 };
 
